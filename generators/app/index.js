9430dee93ba4e2564479b9890d9b5a55b9a9e759
Philipp Kitzberger <philipp@kitze.net>
2018-10-12 17:33:54 +0200

    FIX: typo

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,40 +1,37 @@
-'use strict'
+'use strict';
 
-const BaseGenerator = require('../base.js')
-    , yosay = require('yosay')
-    , chalk = require('chalk')
-    , glob = require('glob')
+const BaseGenerator = require('../base.js');
+const chalk = require('chalk');
+const glob = require('glob');
 
 module.exports = class extends BaseGenerator {
-
   async prompting() {
-    await super.prompting()
+    await super._promptingBasic();
   }
 
   writing() {
-    var files = glob.sync('*', {dot: true, nodir: true, cwd: this.templatePath()})
+    var files = glob.sync('*', { dot: true, nodir: true, cwd: this.templatePath() });
 
     if (files.length) {
-      this.log(chalk.green('Creating ' + files.length + ' files'))
+      this.log(chalk.green('Creating ' + files.length + ' files'));
 
-      for (var i=0; i<files.length; i++) {
-        var targetName = files[i].replace(/^[_]/, '')
-        this.log('Creating ' + targetName)
+      for (var i = 0; i < files.length; i++) {
+        var targetName = files[i].replace(/^[_]/, '');
+        this.log('Creating ' + targetName);
         this.fs.copyTpl(
           this.templatePath(files[i]),
           this.destinationPath(targetName),
           this.config.getAll()
-        )
+        );
       }
     } else {
-      this.log(chalk.red('No templates files found!'))
+      this.log(chalk.red('No templates files found!'));
     }
   }
 
-  install() {
-  }
+  install() {}
 
   end() {
-    this.log(chalk.green('Done with everything!'))
+    this.log(chalk.green('Done with everything!'));
   }
-}
+};
