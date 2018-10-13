c53c0d2334abc0766ad9d5df1bc16daa7ef6dc5c
Philipp Kitzberger <philipp@kitze.net>
2018-10-13 12:51:54 +0200

    Add colored output to extbase controller

--- generators/cli/templates/Classes/Command/CommandController.php
+++ generators/cli/templates/Classes/Command/CommandController.php
@@ -16,9 +16,20 @@ class <%- controller %>CommandController extends CommandController
 	public function <%- command %>Command($pid = null, $dryRun = false)
 	{
 		if (is_null($pid)) {
-			echo 'Please specify pid!' . "\n";
+			$this->outputWarning('Please specify pid!' . "\n");
 			exit;
 		}
 	}
 
+	protected function outputError($text, array $arguments = array())
+	{
+		$text =  "\033[0;31m" . $text . "\033[0m";
+		return $this->output($text, $arguments);
+	}
+
+	protected function outputWarning($text, array $arguments = array())
+	{
+		$text =  "\033[0;33m" . $text . "\033[0m";
+		return $this->output($text, $arguments);
+	}
 }
