61cbc7de72b8a04fdb2a47721a30511149fd9935
Philipp Kitzberger <philipp@kitze.net>
2019-03-08 08:27:48 +0100

    Added upsert functions to command controller

--- generators/cli/templates/Classes/Command/CommandController.php
+++ generators/cli/templates/Classes/Command/CommandController.php
@@ -32,4 +32,108 @@ class <%- controller %>CommandController extends CommandController
 		$text =  "\033[0;33m" . $text . "\033[0m";
 		return $this->output($text, $arguments);
 	}
+
+	protected function runDbOperation()
+	{
+		// TODO: create $this->records
+
+		$tableName = 'xxxxxxx';
+
+		try {
+			$this->sqlQuery('START TRANSACTION');
+
+			$this->deleteAllRecordsBeforehand($tableName);
+
+			foreach ($this->records as $record) {
+				$this->upsertRecord($tableName, $record);
+			}
+
+			$this->sqlQuery('COMMIT');
+		} catch (\Exception $e) {
+			$this->sqlQuery('ROLLBACK');
+			throw $e;
+		}
+	}
+
+	/**
+	 * Upserts a record
+	 *
+	 * @param  string $tableName
+	 * @param  array $record
+	 *
+	 * @return mixed
+	 */
+	protected function upsertRecord($tableName, $record)
+	{
+		return $this->upsert(
+			$tableName,
+			[
+				'identifier' => (int)$record['identifier'],
+				'title' => $GLOBALS['TYPO3_DB']->fullQuoteStr($record['title'], $tableName),
+				'url' => $GLOBALS['TYPO3_DB']->fullQuoteStr($record['url'], $tableName),
+				'crdate' => $_SERVER['REQUEST_TIME'],
+				'tstamp' => $_SERVER['REQUEST_TIME'],
+			],
+			[
+				'identifier=VALUES(identifier)',
+				'title=VALUES(title)',
+				'url=VALUES(url)',
+				'tstamp=VALUES(tstamp)',
+				'deleted=0',
+			]
+		);
+	}
+
+	/**
+	 * Upserts a table record
+	 *
+	 * @param  string $tableName
+	 * @param  array $insertData
+	 * @param  array $updateData
+	 *
+	 * @return mixed
+	 */
+	protected function upsert($tableName, $insertData, $updateData)
+	{
+		$query = sprintf(
+			'INSERT INTO %s (%s) VALUES (%s) ON DUPLICATE KEY UPDATE %s',
+			$tableName,
+			implode(',', array_keys($insertData)),
+			implode(',', $insertData),
+			implode(',', $updateData)
+		);
+
+		return $this->sqlQuery($query);
+	}
+
+	/**
+	 * Soft-deletes all records before importing them again
+	 *
+	 * @return void
+	 */
+	protected function deleteAllRecordsBeforehand($table, $where = '1=1')
+	{
+		$GLOBALS['TYPO3_DB']->exec_UPDATEquery($table, $where, ['deleted' => 1, 'tstamp' => $_SERVER['REQUEST_TIME']]);
+	}
+
+	/**
+	 * SQL Query (with exception support)
+	 *
+	 * @param string $query SQL query
+	 *
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	protected function sqlQuery($query)
+	{
+		$ret = $GLOBALS['TYPO3_DB']->sql_query($query);
+		if (!$ret) {
+			throw new \Exception(sprintf(
+				'SQL-Error: %s [%s]',
+				$GLOBALS['TYPO3_DB']->sql_error(),
+				$GLOBALS['TYPO3_DB']->sql_errno()
+			));
+		}
+		return $ret;
+	}
 }
