1cbcb2566ad322e1a4783ed098565c71a48834ed
Philipp Kitzberger <philipp@kitze.net>
2019-04-15 04:29:19 +0200

    Improve symfony command controller

--- generators/cli/templates/Configuration/Commands.php
+++ generators/cli/templates/Configuration/Commands.php
@@ -2,6 +2,6 @@
 
 return [
     '<%- ext_key %>:<%- command %>' => [
-        'class' => \<%- VendorName %>\<%- ExtKey %>\<%- Command %>Command::class
+        'class' => \<%- VendorName %>\<%- ExtKey %>\Command\<%- Command %>Command::class
     ],
 ];
